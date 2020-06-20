ea56335dd9c2764062a31838da1fe3315c81d9b0
Abraham <abrahamnunez106101@gmail.com>
2020-06-20 16:47:15 -0500

    react-router-101

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true
     }
 }
